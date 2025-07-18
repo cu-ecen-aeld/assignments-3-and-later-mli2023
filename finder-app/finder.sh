8b1f04e99623faf214fffe7507b17d3c626b0621
Matt.Li <mli@ampac.net>
2025-07-18 15:40:29 +0800

    add some self test

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -29,3 +29,41 @@ num_files=$(echo "$matching_files" | wc -l)
 # Count the number of matching lines in all files
 num_matching_lines=$(grep -o "$searchstr" $matching_files | wc -l)
 echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
+
+#https://stackoverflow.com/questions/18135451/what-is-the-difference-between-var-var-and-var-in-the-bash-shell
+var="foo bar"
+for i in "${var}dddd"; do # Expands to 'for i in "foo bar"; do...'
+    echo $i         #   so only runs the loop once
+done
+
+#array example
+foo=(a b c)
+# Returns number of elements in array.
+$ echo ${#foo[*]}
+echo ${foo}
+#a
+
+echo -e "\n"
+echo ${foo[@]}
+# a b c
+
+echo -e "\n"
+echo $foo[@]
+# a[@]
+
+foo=("the first" "the second")
+for i in "${foo[@]}"; do # Expands to 'for i in "the first" "the second"; do...'
+    echo $i              #   so the loop runs twice
+done
+# the first
+# the second
+
+echo -e "\n"
+foo=("the first" "the second")
+for i in ${foo[@]}; do # Expands to 'for i in the first the second; do...'
+    echo $i            #   so the loop runs four times!
+done
+# the
+# first
+# the
+# second
